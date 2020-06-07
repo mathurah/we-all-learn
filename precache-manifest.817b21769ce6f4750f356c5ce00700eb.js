b1a6ad70ce08c203fa4c0bb1e1a000588c81285a
mathurahravigulan <mathurah.ravigulan@edu.uwaterloo.ca>
2020-06-06 23:14:21 -0400

    Updates

---
+++ precache-manifest.817b21769ce6f4750f356c5ce00700eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "205357a3ed96e2c574dc0566a9a6f77e",
+    "url": "/we-all-learn/index.html"
+  },
+  {
+    "revision": "25bbec7e223f225b475f",
+    "url": "/we-all-learn/static/css/2.977fc06e.chunk.css"
+  },
+  {
+    "revision": "1455a85ff3fd3b28e476",
+    "url": "/we-all-learn/static/css/main.46ecfeda.chunk.css"
+  },
+  {
+    "revision": "25bbec7e223f225b475f",
+    "url": "/we-all-learn/static/js/2.2bb28a17.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/we-all-learn/static/js/2.2bb28a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1455a85ff3fd3b28e476",
+    "url": "/we-all-learn/static/js/main.1a346d9d.chunk.js"
+  },
+  {
+    "revision": "110ec553d63729ee571d",
+    "url": "/we-all-learn/static/js/runtime-main.b6882673.js"
+  }
+]);
